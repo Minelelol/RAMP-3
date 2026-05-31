4976dcd7a305f78d4a6c47dbb064acaa2b73d254
Minelelol <89744342+Minelelol@users.noreply.github.com>
2026-05-31 22:52:34 +0700

    Rename Shader Update as Shader Recompile

--- build_shader_recompile.bat
+++ build_shader_recompile.bat
@@ -2,7 +2,7 @@ cd TagTool
 (
   Type ..\setvariabledirectory.cmds
   echo(
-  echo RunCommands workingdir\build_shader_updates.cmds
+  echo RunCommands workingdir\build_shader_recompile.cmds
 ) | TagTool.exe
 
 pause
